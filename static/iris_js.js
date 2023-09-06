7d125b144d633b6e14e6f7e6e10610fa3e2d0c18
Madhavaraj-s-p <133520792+Madhavaraj-s-p@users.noreply.github.com>
2023-09-06 13:30:08 +0530

    Add files via upload

---
+++ static/iris_js.js
@@ -0,0 +1,13 @@
+function inputs()
+{
+    const Sepel_Length = document.getElementById('Sepel_Length').value;
+    const Sepel_Width = document.getElementById('Sepel_Width').value;
+    const Petal_Length = document.getElementById('Petal_Length').value;
+    const Petal_Width = document.getElementById('Petal_Width').value;
+    console.log(Sepel_Length);
+    console.log(Sepel_Width);
+    console.log(Petal_Length);
+    console.log(Petal_Width);
+
+}
+
